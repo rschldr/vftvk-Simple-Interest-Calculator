90a7be185ca56e1bdfcbdbf2528b726a5a1e788b
Renata Schleder <nana.schleder@gmail.com>
2022-02-16 16:48:24 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ function compute()
     p = document.getElementById("principal").value;
     
 }
+
+function getSliderValue() {
+    document.getElementById("rate_val").innerHTML = document.getElementById("rate").value;
+}
         
